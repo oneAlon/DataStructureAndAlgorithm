0a5fdb01a78adda4d478c3eeec5f0282cbb58121
xygj <wangyalong24@126.com>
2018-12-21 11:26:56 +0800

    项目目录重构

---
+++ DataStructure/DataStructure/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DataStructure
+//
+//  Created by xygj on 2018/12/21.
+//  Copyright © 2018 OneAlon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- Sort/Sort/main.m
+++ Sort/Sort/main.m
@@ -1,15 +1,16 @@
 //
 //  main.m
-//  SortDemo
+//  Sort
 //
-//  Created by xygj on 2018/12/20.
-//  Copyright © 2018 xygj. All rights reserved.
+//  Created by xygj on 2018/12/21.
+//  Copyright © 2018 OneAlon. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        // insert code here...
         NSLog(@"Hello, World!");
     }
     return 0;
