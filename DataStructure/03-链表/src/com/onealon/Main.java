a041171d4e6d784ba3e490e88a1d51ab8ab6497e
xygj <wangyalong24@126.com>
2019-06-18 11:45:55 +0800

    单向链表

--- DataStructure/03-链表/src/com/onealon/Main.java
+++ DataStructure/03-链表/src/com/onealon/Main.java
@@ -8,8 +8,32 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("链表");
-        System.out.println("Main.main");
-        System.out.println("args = " + args);
+        List<Integer> list = new LinkList<>();
+        testList(list);
+    }
+
+    static void testList(List<Integer> list) {
+        list.add(11);
+        list.add(22);
+        list.add(33);
+        list.add(44);
+
+        list.add(0, 55); // [55, 11, 22, 33, 44]
+        list.add(2, 66); // [55, 11, 66, 22, 33, 44]
+        list.add(list.size(), 77); // [55, 11, 66, 22, 33, 44, 77]
+
+        list.remove(0); // [11, 66, 22, 33, 44, 77]
+        list.remove(2); // [11, 66, 33, 44, 77]
+        list.remove(list.size() - 1); // [11, 66, 33, 44]
+
+        AssertTool.test(list.indexOf(44) == 3);
+        AssertTool.test(list.indexOf(22) == List.ELEMENT_NOT_FOUND);
+        AssertTool.test(list.contains(33));
+        AssertTool.test(list.get(0) == 11);
+        AssertTool.test(list.get(1) == 66);
+        AssertTool.test(list.get(list.size() - 1) == 44);
+
+        System.out.println(list);
     }
 
 }
